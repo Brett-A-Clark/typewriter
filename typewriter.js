21ebf7809e657526beec4f908d61f7efffcbc591
vagrant <b-a-clark@shaw.cagit config --global user.name Brett>
2021-07-27 20:33:49 +0000

    Animated sentence by using setInterval and clearInterval

---
+++ typewriter.js
@@ -0,0 +1,19 @@
+const sentence = "hello there from lighthouse labs";
+
+let i = 0;
+
+const returnValue = setInterval(() => {
+  // console.log(sentence[i]);
+  if ( i < sentence.length) {
+    process.stdout.write(sentence[i]);
+    i++;
+    // console.log(returnValue);
+  } else {
+    clearInterval(returnValue)
+    console.log("");
+  }
+}, 50);
+
+
+
+// for (const char of sentence[i]) {
